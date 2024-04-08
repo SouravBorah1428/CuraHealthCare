3042b6ac0c5fa8eeab4b7ed9943ed9c77be21713
SOURAV BORAH <souravborah1428@gmail.com>
2024-04-08 21:47:51 +0530

    updated BaseClass.java, added Dockerfile and added helper classes

---
+++ src/main/java/org/cura/utils/TestConstants.java
@@ -0,0 +1,8 @@
+package org.cura.utils;
+
+public class TestConstants {
+    public final static int PAGE_LOAD_TIMEOUT = 10;
+    public final static int IMPLICIT_WAIT = 10;
+    public final static int SCROLL_DOWN = 75;
+    public final static int SCROLL_UP = -75;
+}

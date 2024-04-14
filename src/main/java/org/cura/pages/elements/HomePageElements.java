d8001f7c899198241752c224653f41d5bf1a1a72
SOURAV BORAH <souravborah1428@gmail.com>
2024-04-14 20:41:49 +0530

    New changes added

--- src/main/java/org/cura/pages/elements/HomePageElements.java
+++ src/main/java/org/cura/pages/elements/HomePageElements.java
@@ -1,4 +1,24 @@
 package org.cura.pages.elements;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class HomePageElements {
+    @FindBy (xpath = "//select[@id='combo_facility']")
+    public WebElement facilityDropDown;
+
+    @FindBy (xpath = "//input[@id='chk_hospotal_readmission']")
+    public WebElement hospitalReadmissionCheckbox;
+
+    @FindBy (xpath = "//label[@class='radio-inline'][1]//input")
+    public WebElement healthCareProgramRadioButtons;
+
+    @FindBy (xpath = "//input[@id='txt_visit_date']")
+    public WebElement visitDateField;
+
+    @FindBy (xpath = "//textarea[@id='txt_comment']")
+    public WebElement commentTextArea;
+
+    @FindBy (xpath = "//button[@id='btn-book-appointment']")
+    public WebElement bookAppointmentBtn;
 }
--- src/main/java/org/cura/pages/functions/HomePage.java
+++ src/main/java/org/cura/pages/functions/HomePage.java
@@ -1,7 +1,55 @@
 package org.cura.pages.functions;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.cura.base.BaseClass;
 import org.cura.pages.elements.HomePageElements;
+import org.cura.utils.WebDriverUtility;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class HomePage extends HomePageElements {
+    static final Logger logger = LogManager.getLogger(HomePage.class.getName());
+    private WebDriverUtility driverUtility;
 
+    public HomePage() {
+        PageFactory.initElements(BaseClass.driver, this);
+        driverUtility = new WebDriverUtility(BaseClass.driver);
+    }
+
+    public String getPageTitle() {
+        logger.info("Getting page title");
+        return driverUtility.getPageTitle();
+    }
+
+    public void selectFacility() {
+        Select select = new Select(facilityDropDown);
+        logger.info("Selecting dropdown value from the facility dropdown");
+        select.selectByValue("Hongkong CURA Healthcare Center");
+    }
+
+    public void checkHospitalReadmission() {
+        logger.info("Checking Apply for Hospital Readmission checkbox");
+        driverUtility.click(hospitalReadmissionCheckbox);
+    }
+
+    public void selectHealthCareProgram() {
+        logger.info("Selecting health care program from the radio buttons");
+        driverUtility.click(healthCareProgramRadioButtons);
+    }
+
+    public void enterVisitDate() {
+        logger.info("Entering visit date");
+        driverUtility.type(visitDateField, "28/04/2024");
+    }
+
+    public void enterComments() {
+        logger.info("Enter comments in the comment box");
+        driverUtility.type(commentTextArea, "Booking for 28th of April");
+    }
+
+    public void bookAppointment() {
+        logger.info("Clicking on Book Appointment button");
+        driverUtility.click(bookAppointmentBtn);
+    }
 }

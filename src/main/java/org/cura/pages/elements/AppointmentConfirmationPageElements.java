269f5259407e224a65d283fddfe3d409b4e4d08c
SOURAV BORAH <souravborah1428@gmail.com>
2024-04-15 22:36:57 +0530

    Minor additions

--- src/main/java/org/cura/pages/elements/AppointmentConfirmationPageElements.java
+++ src/main/java/org/cura/pages/elements/AppointmentConfirmationPageElements.java
@@ -1,4 +1,7 @@
 package org.cura.pages.elements;
 
+import org.openqa.selenium.WebElement;
+
 public class AppointmentConfirmationPageElements {
+    public WebElement successMessage;
 }
--- src/main/java/org/cura/pages/functions/AppointmentConfirmationPage.java
+++ src/main/java/org/cura/pages/functions/AppointmentConfirmationPage.java
@@ -1,6 +1,11 @@
 package org.cura.pages.functions;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.cura.pages.elements.AppointmentConfirmationPageElements;
+import org.cura.utils.WebDriverUtility;
 
 public class AppointmentConfirmationPage extends AppointmentConfirmationPageElements {
+    static final Logger logger = LogManager.getLogger(AppointmentConfirmationPage.class);
+    public WebDriverUtility driverUtility;
 }
